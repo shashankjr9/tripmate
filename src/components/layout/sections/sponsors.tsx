ad61aa473bdbdaaf4acbbd2e285d7d8bdea3ac12
Dimitrios Pertsinidis <dpertsin@gmail.com>
2024-11-11 10:18:28 +0200

    delete unnecessary icons

--- src/components/layout/sections/sponsors.tsx
+++ src/components/layout/sections/sponsors.tsx
@@ -1,41 +1,31 @@
 "use client";
 
-import { Icon } from "@/components/ui/icon";
 import { Marquee } from "@devnomic/marquee";
 import "@devnomic/marquee/dist/index.css";
-import { icons } from "lucide-react";
 interface sponsorsProps {
-  icon: string;
   name: string;
 }
 
 const sponsors: sponsorsProps[] = [
   {
-    icon: "Crown",
     name: "Gemini Ai",
   },
   {
-    icon: "Vegan",
     name: "Google Cloud",
   },
   {
-    icon: "Ghost",
     name: "Google Maps",
   },
   {
-    icon: "Puzzle",
     name: "Firebase",
   },
   {
-    icon: "Squirrel",
     name: "React",
   },
   {
-    icon: "Cookie",
     name: "Vite",
   },
   {
-    icon: "Drama",
     name: "Tailwind CSS",
   },
 ];
@@ -43,9 +33,7 @@ const sponsors: sponsorsProps[] = [
 export const SponsorsSection = () => {
   return (
     <section id="sponsors" className="max-w-[75%] mx-auto pb-24 sm:pb-32">
-      <h2 className="text-lg md:text-xl text-center mb-6">
-        Our Tools
-      </h2>
+      <h2 className="text-lg md:text-xl text-center mb-6">Our Tools</h2>
 
       <div className="mx-auto">
         <Marquee
@@ -54,17 +42,11 @@ export const SponsorsSection = () => {
           innerClassName="gap-[3rem]"
           pauseOnHover
         >
-          {sponsors.map(({ icon, name }) => (
+          {sponsors.map(({ name }) => (
             <div
               key={name}
               className="flex items-center text-xl md:text-2xl font-medium"
             >
-              <Icon
-                name={icon as keyof typeof icons}
-                size={32}
-                color="white"
-                className="mr-2"
-              />
               {name}
             </div>
           ))}
